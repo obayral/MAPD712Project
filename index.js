d2806d9a9b3e32955a9be9a70b75333c4857a4ad
OguzBayral <oguzbayral93@gmail.com>
2018-12-08 12:27:52 -0500

    Handled the POST requests within the website

--- index.js
+++ index.js
@@ -1,5 +1,8 @@
 var express = require('express');
+var bodyParser = require('body-parser');
 var app = express();
+var urlencodedParser = bodyParser.urlencoded({ extended: false })
+
 var PORT = process.env.PORT || 3000;
 
 app.use('/assets',express.static('assets'));
@@ -11,14 +14,25 @@ app.get('/',function(req,res){
     res.render("index");
 });
 
+app.get('/home',function(req,res){
+    res.render("index");
+});
+
 app.get('/contact',function(req,res){
     res.render("contact");
 });
 
+app.post('/contact',urlencodedParser,function(req,res){
+    console.log(req.body);
+    res.render("contact-success",{data: req.body});
+});
+
 app.get('/about',function(req,res){
+    
     res.render("about");
 });
 
 app.listen(PORT,function(){
     console.log('Server is listening at ' +  PORT);
 });
+
