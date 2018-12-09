42fc701ccfc187a47dda0eb9e7eb0cc6a77f107f
OguzBayral <oguzbayral93@gmail.com>
2018-12-08 19:03:33 -0500

    Implemented mongodb and mlab integration with the server

---
+++ controllers/appControllers.js
@@ -0,0 +1,95 @@
+var bodyParser = require('body-parser');
+var urlencodedParser = bodyParser.urlencoded({ extended: false });
+var mongoose = require("mongoose");
+var getRequestCounter = 0;
+var postRequestCounter = 0;
+var putRequestCounter = 0;
+var deleteRequestCounter = 0;
+
+var uristring = 'mongodb://hacix:hacix123@ds129454.mlab.com:29454/mapd712';
+
+// Makes connection asynchronously.  Mongoose will queue up database
+// operations and release them when the connection is complete.
+mongoose.connect(uristring, function (err, res) {
+  if (err) { 
+    console.log ('ERROR connecting to: ' + uristring + '. ' + err);
+  } else {
+    console.log ('Successfully connected to: ' + uristring);
+  }
+});
+var patientSchema = new mongoose.Schema({
+    first_name: String,
+    last_name: String, 
+    date_of_birth: String,
+    address: String,
+    mail_address: String,
+    date_admitted: String,
+    department: String,
+    ailment: String
+  });
+var Patient = mongoose.model('Patient', patientSchema);
+
+module.exports = function(app){
+    app.get('/',function(req,res){
+        getRequestCounter++;
+        console.log("Processed Request Counter --> GET: " +  getRequestCounter + ", POST: " + postRequestCounter + ", PUT: " + putRequestCounter +", DELETE: " +deleteRequestCounter);
+        res.render("index");
+    });
+    app.get('/home',function(req,res){
+        getRequestCounter++;
+        console.log("Processed Request Counter --> GET: " +  getRequestCounter + ", POST: " + postRequestCounter + ", PUT: " + putRequestCounter +", DELETE: " +deleteRequestCounter);
+        res.render("index");
+    });
+   app.get('/contact',function(req,res){
+        console.log("CONTACT PAGE");
+        getRequestCounter++;
+        console.log("Processed Request Counter --> GET: " +  getRequestCounter + ", POST: " + postRequestCounter + ", PUT: " + putRequestCounter +", DELETE: " +deleteRequestCounter);
+        res.render("contact");
+    });
+    app.post('/contact',urlencodedParser,function(req,res){
+        console.log("CONTACT SUCCESS");
+        console.log(req.body);
+        getRequestCounter++;
+        console.log("Processed Request Counter --> GET: " +  getRequestCounter + ", POST: " + postRequestCounter + ", PUT: " + putRequestCounter +", DELETE: " +deleteRequestCounter);
+        res.render("contact-success",{data: req.body});
+    });
+    app.get('/about',function(req,res){
+        console.log("ABOUT PAGE");
+        getRequestCounter++;
+        console.log("Processed Request Counter --> GET: " +  getRequestCounter + ", POST: " + postRequestCounter + ", PUT: " + putRequestCounter +", DELETE: " +deleteRequestCounter);
+        res.render("about");
+    });
+
+    app.get('/newpatient',function(req,res){
+        console.log("NEW PATIENT PAGE");
+        getRequestCounter++;
+        console.log("Processed Request Counter --> GET: " +  getRequestCounter + ", POST: " + postRequestCounter + ", PUT: " + putRequestCounter +", DELETE: " +deleteRequestCounter);
+        res.render("newpatient");
+    });
+    
+    app.post('/newpatient',urlencodedParser,function(req,res){
+        postRequestCounter++;
+        console.log("Processed Request Counter --> GET: " +  getRequestCounter + ", POST: " + postRequestCounter + ", PUT: " + putRequestCounter +", DELETE: " +deleteRequestCounter);
+        console.log(req.body);
+        var newPatient = Patient(req.body).save(function(err,data){
+            if (err) throw err;
+            res.render("addedpatient",{data: req.body});
+        })
+    });
+
+    // Get all patients in the system
+    app.get('/patients', function (req, res, next) {
+        console.log("ALL PATIENTS PAGE");
+        getRequestCounter++;
+        console.log("Processed Request Counter --> GET: " +  getRequestCounter + ", POST: " + postRequestCounter + ", PUT: " + putRequestCounter +", DELETE: " +deleteRequestCounter);
+        Patient.find({},function(err, data){
+            if(err) throw err;
+            res.send(data)
+        });
+        
+        console.log('received GET request.');
+    })
+  
+
+
+}
--- index.js
+++ index.js
@@ -1,36 +1,15 @@
 var express = require('express');
-var bodyParser = require('body-parser');
+var appController = require('./controllers/appControllers')
+
 var app = express();
-var urlencodedParser = bodyParser.urlencoded({ extended: false })
+
 
 var PORT = process.env.PORT || 3000;
 
 app.use('/assets',express.static('assets'));
+appController(app);
 
 app.set('view engine', 'ejs');
-
-
-app.get('/',function(req,res){
-    res.render("index");
-});
-
-app.get('/home',function(req,res){
-    res.render("index");
-});
-
-app.get('/contact',function(req,res){
-    res.render("contact");
-});
-
-app.post('/contact',urlencodedParser,function(req,res){
-    console.log(req.body);
-    res.render("contact-success",{data: req.body});
-});
-
-app.get('/about',function(req,res){
-    
-    res.render("about");
-});
 
 app.listen(PORT,function(){
     console.log('Server is listening at ' +  PORT);
